07d8ba59e23cd4023dd98e39d5ff58697d74efa6
flying_rabbit <1290830173@qq.com>
2020-06-30 00:24:07 +0800

    ssa lzh

---
+++ ssa/ssa.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <set>
+#include "intermediatecode.h"
+
+class basicBlock {
+public:
+	int number;
+	int start;
+	int end;
+	std::set<int> pred;				// 前驱
+	std::set<int> succeeds;			// 后继节点
+	std::set<int> domin;				// 必经节点
+	std::set<int> idom;				// 直接必经节点
+	std::set<int> tmpIdom;			// 计算直接必经节点算法中需要的数据结构
+	basicBlock(int number, int start, int end, std::set<int> pred, std::set<int> succeeds) {
+		this->number = number;
+		this->start = start;
+		this->end = end;
+		this->pred = pred;
+		this->succeeds = succeeds;
+	}
+};
+
+class SSA {
+private:
+	std::vector<std::vector<CodeItem>> codetotal;
+	std::vector<std::vector<int>> blockOrigin;
+	std::vector<std::vector<basicBlock>> blockCore;
+	void divide_basic_block();
+	void build_dom_tree();
+	void build_idom_tree();
+public:
+	SSA(std::vector<std::vector<CodeItem>> codetotal) {
+		this->codetotal = codetotal;
+	}
+	void generate();
+};
