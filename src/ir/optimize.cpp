0254353a6ff6c4dbb3dbf2fbb6c5f2f2ca6c640c
Xiaomaoaichiyu <1844061236@qq.com>
2020-08-02 18:06:38 +0800

    update

--- src/ir/optimize.cpp
+++ src/ir/optimize.cpp
@@ -558,17 +558,17 @@ void irOptimize() {
 
 		
 		//计算活跃变量
-		/*codetotal = LIR;
+		codetotal = LIR;
 		TestIrCode("ly1.txt");
 		SSA ssa1;
 		ssa1.generate_activeAnalyse();
 		ssa1.get_avtiveAnalyse_result();
-		ly_act.print_ly_act();*/
+		ly_act.print_ly_act();
 
 		//寄存器直接指派
-		registerAllocation();
+		//registerAllocation();
 
-		//registerAllocation2(ssa1.getblocks());
+		registerAllocation2(ssa1.getblocks());
 
 		printLIR("armIR.txt");
 
