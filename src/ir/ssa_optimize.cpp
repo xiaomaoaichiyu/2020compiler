8547df8401e82bf5a3be933bd4b0b144981610af
flying_rabbit <1290830173@qq.com>
2020-08-14 19:21:42 +0800

    函数内联参数不允许函数调用

--- src/ir/ssa_optimize.cpp
+++ src/ir/ssa_optimize.cpp
@@ -635,12 +635,9 @@ void SSA::inline_function() {
 						/*else if (globalRegAllocated[i] == 8) {	// 只有sort样例中不内联
 							continue;
 						}*/
-						else {
-							cout << "在函数 " << funNum2Name[i] << " 中内联函数 " << funName << endl;
-						}
 						bool ifCallAsParam = false;
 						for (int iter2 = j + 1; iter2 < codetotal[i].size(); iter2++) {
-							CodeItem ttt = codetotal[i][j];
+							CodeItem ttt = codetotal[i][iter2];
 							if (ttt.getCodetype() == NOTE
 								&& (ttt.getOperand1().compare("func") == 0 || ttt.getOperand1().compare("inline") == 0)) {
 								if (ttt.getResult().compare(funName) != 0 || ttt.getOperand2().compare("begin") == 0) {
@@ -650,6 +647,7 @@ void SSA::inline_function() {
 							}
 						}
 						if (ifCallAsParam) continue;	// 函数调用作为参数
+						cout << "在函数 " << funNum2Name[i] << " 中内联函数 " << funName << endl;
 					}
 
 					alreadyNeilian.insert(funNum);		//丛睿轩添加的
